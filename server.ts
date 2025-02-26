4b227f9b499ccfa819e5066b67eae2dc1a52e709
PasinduMshan <madushan01722@gmail.com>
2025-02-26 16:49:17 +0530

    update server.ts and fix bugs in that file

--- server.ts
+++ server.ts
@@ -1,30 +1,10 @@
-// import express from 'express';
-// import fieldRoutes from "./routes/field-routes";
-//
-// const app = express();
-//
-// app.use(express.json());
-// app.use('/',(req,res,next)=>{
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-type');
-//
-//     next();
-// })
-//
-// app.use('/field', fieldRoutes)
-//
-// app.listen(3000, (err=>{
-//     console.log("Server running on port 3000");
-// }));
-//
-// app.use('/',(req,res,next)=>{
-//     res.status(200).send('Not Found');
-// })
-
 import express from 'express';
 import fieldRoutes from "./routes/field-routes";
 import staffRoutes from "./routes/staff-routes";
+import cropRoutes from "./routes/crop-routes";
+import monitoringlogRoutes from "./routes/monitoringlog-routes";
+import vehicleRoutes from "./routes/vehicle-routes";
+import equipmentRoutes from "./routes/equipment-routes";
 
 const app = express();
 
@@ -41,7 +21,10 @@ app.use((req, res, next) => {
 // Routes
 app.use('/field', fieldRoutes);
 app.use('/staff', staffRoutes);
-app.use('/crop', staffRoutes);
+app.use('/crop', cropRoutes);
+app.use('/monitoringLog', monitoringlogRoutes);
+app.use('/vehicle', vehicleRoutes);
+app.use('/equipment', equipmentRoutes);
 
 // 404 Not Found Middleware
 app.use((req, res) => {
